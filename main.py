9c60d1806d39b4a53161930e0b328db575b47491
Adam Abdallah <134047752+Adamabdallah1@users.noreply.github.com>
2023-06-15 15:14:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+import smtplib
+
+APP_PASS = "Your APP Password"
+SENDER = "Sender email"
+RECEIVER = "receiver email"
+BUY_PRICE = 200
+SMPT_ADDRESS = "your smpt address"
+# The URL of the product you want to track
+url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+# Add your browser headers
+header = {
+    "User-Agent": "User Agent header",
+    "Accept-Language": "Language header"
+}
+
+response = requests.get(url, headers=header)
+
+soup = BeautifulSoup(response.content, "lxml")
+print(soup.prettify())
+
+title = soup.find(id="productTitle").get_text().strip()
+price = soup.find(class_="a-offscreen").get_text()
+# print(price)
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+# print(f"Title: {title}:\nPrice: {price_as_float}")
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(SMPT_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(SENDER, APP_PASS)
+        connection.sendmail(
+            from_addr=SENDER,
+            to_addrs=RECEIVER,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
